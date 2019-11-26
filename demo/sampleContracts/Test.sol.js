293fd63bb1f1875384810c2627484d7493b03d0b
ninabreznik <ninabreznik@gmail.com>
2019-11-26 19:52:42 +0700

    Update ethers.js

--- demo/sampleContracts/Test.sol.js
+++ demo/sampleContracts/Test.sol.js
@@ -3,7 +3,7 @@ pragma solidity ^0.5.3;
 
 contract Test {
 
-    function double(uint i) public returns (uint) {
+    function double(uint i) public payable returns (uint) {
 
         return i*2;
     }
